eb2cd3b223036dbb7fd17c9ce75fa744abe4b0a8
Dmitry Savelev <dsavelev1703@yandex.ru>
2023-03-24 01:58:02 +0700

    ppppppp

--- app.js
+++ app.js
@@ -8,6 +8,3 @@ app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
   // console.log(`App listening on port ${PORT}`);
 });
-const a = 9;
-console.log(a);
-console.log('dd');
