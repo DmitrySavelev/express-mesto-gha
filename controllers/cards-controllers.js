c83dbe50bd5ff0452b545edfab0d9c571d8a8a40
Dmitry Savelev <dsavelev1703@yandex.ru>
2023-04-03 19:29:43 +0700

    rights of owners

--- controllers/cards-controllers.js
+++ controllers/cards-controllers.js
@@ -12,7 +12,7 @@ const getCards = (req, res) => {
     .catch(() => res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка по умолчанию' }));
 };
 
-const createCard = (req, res) => {
+const createCard = (req, res) => { // POST /cards
   const owner = req.user._id;
   const { name, link } = req.body;
   Card.create({ name, link, owner })
@@ -27,14 +27,18 @@ const createCard = (req, res) => {
     });
 };
 
-const deleteCard = (req, res) => {
+const deleteCard = (req, res) => { // DELETE /cards/:cardId
   Card
-    .findByIdAndDelete(req.params.cardId)
+    .findById(req.params.cardId)
     .then((card) => {
       if (!card) {
         return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Карточка с указанным _id не найдена' });
       }
-      return res.send({ data: card });
+      if (card.owner.toString() !== req.user._id) {
+        return res.status(403).send({ message: 'Недостаточно прав для удаления карточки' });
+      }
+      return Card.findByIdAndRemove(req.params.cardId)
+        .then((deletedCard) => res.status(200).send({ deletedCard }));
     })
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -45,6 +49,30 @@ const deleteCard = (req, res) => {
       return res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка по умолчанию' });
     });
 };
+
+// const deleteCard = (req, res) => { // DELETE /cards/:cardId
+//   console.log('req>>>>>>>>>>', req.params);
+//   Card
+//     .findByIdAndDelete(req.params.cardId)
+//     .then((card) => {
+//       if (!card) {
+//         return res.status(NOT_FOUND_ERROR_CODE)
+// .send({ message: 'Карточка с указанным _id не найдена' });
+//       }
+//       if (card.owner !== req.user._id) {
+//         return res.status(403).send({ message: 'Недостаточно прав для удаления карточки' });
+//       }
+//       return res.send({ data: card });
+//     })
+//     .catch((err) => {
+//       if (err.name === 'CastError') {
+//         return res
+//           .status(INCORRECT_DATA_ERROR_CODE)
+//           .send({ message: 'Переданы некорректные данные карточки' });
+//       }
+//       return res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка по умолчанию' });
+//     });
+// };
 
 const likeCard = (req, res) => {
   Card
