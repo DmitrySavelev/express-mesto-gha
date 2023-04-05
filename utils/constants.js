f137cdc783f75e2230268285aed9697a80032ba7
Dmitry Savelev <dsavelev1703@yandex.ru>
2023-04-05 16:57:54 +0700

    tests 5

--- utils/constants.js
+++ utils/constants.js
@@ -1,12 +1,5 @@
-const INCORRECT_DATA_ERROR_CODE = 400; // переданы некорректные данные
-const NOT_FOUND_ERROR_CODE = 404; // карточка или пользователь не найден.
-const DEFAULT_ERROR_CODE = 500; // ошибка по-умолчанию.
-
 const regex = /^https?:\/\/(www.)?[-a-z0-9:%._+~#=]{1,}\.[a-z0-9()]{1,}([-a-z0-9()\-._~:/?#[\]@!$&'()*+,;=]*)/i;
 
 module.exports = {
   regex,
-  INCORRECT_DATA_ERROR_CODE,
-  NOT_FOUND_ERROR_CODE,
-  DEFAULT_ERROR_CODE,
 };
