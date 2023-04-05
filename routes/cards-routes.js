6a9c261a607aa4dff6afc1f77ec7bf97908182bc
Dmitry Savelev <dsavelev1703@yandex.ru>
2023-04-05 16:45:21 +0700

    tests 4

--- routes/cards-routes.js
+++ routes/cards-routes.js
@@ -12,7 +12,7 @@ const { cardIdValidation, createCardValidation } = require('../middlewares/valid
 router.get('/cards', getCards);
 router.post('/cards', createCardValidation, createCard);
 router.delete('/cards/:cardId', cardIdValidation, deleteCard);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+router.put('/cards/:cardId/likes', cardIdValidation, likeCard);
+router.delete('/cards/:cardId/likes', cardIdValidation, dislikeCard);
 
 module.exports = router;
