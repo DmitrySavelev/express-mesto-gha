5b35c5aabbea0b6e5351c9cfd6c74cf8cdc751e4
Dmitry Savelev <dsavelev1703@yandex.ru>
2023-04-05 16:55:35 +0700

    tests 5

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,19 @@
 const jsonwebtoken = require('jsonwebtoken');
+const AuthDataError = require('../errors/auth-err');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new AuthDataError('Необходима авторизация'));
   }
   const jwt = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jsonwebtoken.verify(jwt, 'very_difficalt_password');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new AuthDataError('Необходима авторизация'));
   }
-
   req.user = payload; // записываем payload в объект запроса
-
   return next(); // пропускаем запрос дальше
 };
 
